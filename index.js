c944a81d9e72755c3e36f808bcf19e39a2074987
Pertoldi <antoine.pertoldi@gmail.com>
2021-10-15 17:23:03 +0200

    ajout ejs

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
 const express = require('express')
 const fs = require('fs')
 const path = require('path')
+const ejs = require('ejs')
 
 const app = express()
-const port= 3000
+const port = 3000
 
-app.use(express.static('public'));
+app.use(express.static('public'));//pour avoir acces au ressources static: image etc...
 
-app.get('/', (req,res) => {
-	res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+// EJS
+app.set('views', './views') //On indique que les views sont dans le dossier ./views
+app.set('view engine', 'ejs') //On indique que notre moteur de vue est ejs
+
+app.get('/', (req, res) => {
+	res.render('index')
 })
 
-app.get('/index.html', (req,res) => {
-	res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+app.get('/about', (req, res) => {
+	res.render('about')
 })
 
-app.get('/about',(req,res) => {
-	res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+app.get('/contact', (req, res) => {
+	res.render('contact')
 })
 
-app.get('/contact',(req,res) => {
-	res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+app.get('/post', (req, res) => {
+	res.render('post')
 })
 
-app.get('/post',(req,res) => {
-	res.sendFile(path.resolve(__dirname, 'pages/post.html'));
-})
 
 app.listen(port, () => {
 	console.log(`Server Express listen on port ${port}`);
